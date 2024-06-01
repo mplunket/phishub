8099f2459b8286a2f212d67f448270e743bad52c
Michael Plunkett <mplunket@gmail.com>
2024-06-01 12:36:56 -0400

    Updated pre formatting for tab display

--- app/songs/[slug]/page.tsx
+++ app/songs/[slug]/page.tsx
@@ -50,7 +50,7 @@ export default async function Song({ params }: { params: { slug: string } }) {
                         </div>
                     </div>
                 </div>
-                {tabContent ? <div className="font-mono whitespace-pre text-[0.51rem] md:text-base">{tabContent}</div> : <h3 className=" py-20 text-center">This song has no content yet.</h3>}
+                {tabContent ? <div className="font-mono whitespace-pre-wrap text-[0.55rem] md:text-base">{tabContent}</div> : <h3 className=" py-20 text-center">This song has no content yet.</h3>}
             </>
         )
 
