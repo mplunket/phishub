c7e81807120965fc7ad5721d8b065f6a88feb9a4
Michael Plunkett <mplunket@gmail.com>
2024-09-10 17:53:34 -0400

    Adjusted site title size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Twitter, Facebook, Youtube } from 'lucide-react'
-import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
 const Footer: React.FC = () => {
