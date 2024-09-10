78a52b572fceafaf4e7832e437347121150eb2cf
Michael Plunkett <mplunket@gmail.com>
2024-09-10 17:50:27 -0400

    Changed site title padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function LandingPage() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-      <h1 className=" text-8xl font-black mb-2 text-primary">phishub</h1>
+      <h1 className="text-8xl font-black m-2 text-primary">phishub</h1>
       <h2 className="text-lg my-4">#1 Site for Phish Guitar Tabs and Video Lessons</h2>
       <div className="my-8"></div>
       <Suspense fallback={<div>Loading...</div>}>
