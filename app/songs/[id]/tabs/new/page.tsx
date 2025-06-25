b6c0f2ce36fbc555a5c12b06f72e4697e931c4b4
Michael Plunkett <mplunket@gmail.com>
2025-06-24 22:43:23 -0400

    refactor: remove unused Input component import from NewTabPage

--- app/songs/[id]/tabs/new/page.tsx
+++ app/songs/[id]/tabs/new/page.tsx
@@ -1,6 +1,5 @@
 import { createTab } from "@/app/actions";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import {
