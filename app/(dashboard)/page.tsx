935d5f1147b1618e67ecd3c0f9c6652b6672ed11
Michael Plunkett <mplunket@gmail.com>
2025-06-29 02:26:57 -0400

    refactor: remove unused DashboardGroup component from app directory

--- app/(dashboard)/page.tsx
+++
@@ -1,8 +0,0 @@
-// This file marks this directory as a Next.js app directory route group
-export default function DashboardGroup({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
